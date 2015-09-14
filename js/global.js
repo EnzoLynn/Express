bd13e4e3c77b0e74ebabec5334020fb4468fbbbe
chenheling <6710582@qq.com>
2015-09-14 17:02:14 +0800

    customjs path

---
+++ js/global.js
@@ -0,0 +1,10 @@
+var host = '127.0.0.1';
+var port = 818;
+
+var apiControllers = {
+	login: '/login/ajaxLogin'
+};
+
+exports.host = host;
+exports.port = port;
+exports.apiControllers= apiControllers;
