12fee88bd99acce5e1ec65e54de4a3cee25f0102
chenheling <6710582@qq.com>
2015-09-14 11:57:38 +0800

    rest api 1.0

---
+++ routes/login.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+var app = express();
+app.use(express.static(path.join(__dirname, 'node_modules')));
+/* GET users listing. */
+router.post('/', function(req, res) {
+
+
+    // res.status(500).send({ error: 'something blew up' }); 
+    var restApi = require('restApi.js'); 
+    restApi.ajax(res, {
+        host: '127.0.0.1',
+        port: 818,
+        path: '/login/ajaxLogin'
+    }, req.body);
+});
+
+module.exports = router;
