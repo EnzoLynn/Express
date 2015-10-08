8e4273048370c997158afb3cc0ca8595bd6fb1cf
chenheling <6710582@qq.com>
2015-10-08 16:59:43 +0800

    10.8 mango

--- routes/testerror.js
+++ routes/testerror.js
@@ -4,13 +4,22 @@ var router = express.Router();
 //var app = express();
 //app.use(express.static(path.join(__dirname, 'node_modules')));
 /* GET users listing. */
+var uri = "mongodb://localhost:27017/test";
+var db = require('mongoskin').db(uri);
+
 router.get('/', function(req, res) {
-	console.log(123);
-    res.render('error', {
-        message: 'error ,,,',
-        error: {status:200}
+	 
+    db.collection('storeCollection').find().toArray(function(err, result) {
+        if (err) throw err;
+        res.render('error', {
+	        message:result[0].segment,
+	        error: {
+	            status: 200
+	        }
+	    });
     });
+    console.log(123);
+    
 });
 
 module.exports = router;
-
