0d0fc04abca9fed5a28c335efdcfbeca8c20bea0
chenheling <6710582@qq.com>
2015-10-09 10:47:05 +0800

    dbhelper

--- js/dbHelper.js
+++ js/dbHelper.js
@@ -2,7 +2,7 @@ var uri = "mongodb://localhost:27017/test";
 var db = require('mongoskin').db(uri);
 
 db.getList = function(collectionName,callback){
-	 db.collection('storeCollection').find().toArray(function(err, result) {
+	 db.collection(collectionName).find().toArray(function(err, result) {
         if (err) throw err;
         if (callback) {callback(err,result)};
         return result;
@@ -10,6 +10,8 @@ db.getList = function(collectionName,callback){
     });
 
 }
+
+ 
 
 
 module.exports = db;
