d9c9c219b3701fb21acbdc6f6c3bbbb1ec6f43e8
chenheling <6710582@qq.com>
2015-10-09 10:57:07 +0800

    dbhelper

--- js/dbHelper.js
+++ js/dbHelper.js
@@ -1,8 +1,9 @@
 var uri = "mongodb://localhost:27017/test";
 var db = require('mongoskin').db(uri);
 
-db.getList = function(collectionName,callback){
-	 db.collection(collectionName).find().toArray(function(err, result) {
+db.getList = function(collectionName,fillter,callback){
+	
+	 db.collection(collectionName).find(fillter).toArray(function(err, result) {
         if (err) throw err;
         if (callback) {callback(err,result)};
         return result;
--- routes/testerror.js
+++ routes/testerror.js
@@ -7,7 +7,8 @@ var router = express.Router();
 var db = require('../js/dbHelper.js');
 
 router.get('/', function(req, res) {
-    db.getList('storeCollection', function(err, result) {
+	//segment:{$regex:/enzol/i}
+    db.getList('storeCollection',{}, function(err, result) {
         if (err) {
             res.render('error', {
                 message: err,
@@ -17,7 +18,7 @@ router.get('/', function(req, res) {
             });
         };
         res.render('error', {
-            message: result[0].segment,
+            message: JSON.stringify(result),//[0].segment,
             error: {
                 status: 200
             }
